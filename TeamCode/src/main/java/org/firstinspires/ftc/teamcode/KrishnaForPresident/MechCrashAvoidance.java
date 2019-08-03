710223b4f30ec3d5b54fe1a6b01a3b7064ea0a31
JAKCODING <jtofounder@gmail.com>
2019-08-03 11:32:40 -0400

    working on mech crash avoidance

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KrishnaForPresident/MechCrashAvoidance.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode.KrishnaForPresident;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcontroller.external.samples.SensorBNO055IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+
+@TeleOp(name = "Mech Crash Avoidance v0.0.1 lol")
+
+public class MechCrashAvoidance extends OpMode {
+
+    DcMotor fLeft, fRight, bLeft, bRight;
+    DistanceSensor leftD, centerD, rightD;
+    BNO055IMU imu;
+    Orientation angles;
+    Acceleration gravity;
+    double angular, lastAngular;
+
+    public void init(){
+
+        fLeft = hardwareMap.dcMotor.get("fLeft");
+        fRight = hardwareMap.dcMotor.get("fRight");
+        bLeft = hardwareMap.dcMotor.get("bLeft");
+        bRight = hardwareMap.dcMotor.get("bRight");
+
+        leftD = hardwareMap.get(DistanceSensor.class, "leftD");
+        centerD = hardwareMap.get(DistanceSensor.class, "centerD");
+        rightD = hardwareMap.get(DistanceSensor.class, "rightD");
+
+        //Set up imu parameters
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // Start the logging of measured acceleration
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+
+        lastAngular = 0;
+
+    }
+
+    public void loop() {
+
+    }
+
+    public void stop() {
+
+    }
+
+}
